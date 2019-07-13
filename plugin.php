61f0ce6f074fdd7f25adf9c1a6cca83c073ce5d4
Edi <egoetschel@clickwork.ch>
2019-07-14 00:07:34 +0200

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,30 @@
+<?php
+
+class pluginContentID extends Plugin {
+
+	public function form()
+	{
+		global $L;
+
+		$html  = '<div>';
+		$html .= 'There are not settings for this plugin at the moment.';
+		$html .= '</div>';
+
+		return $html;
+	}
+
+	public function adminBodyEnd() {
+
+ 		$html = PHP_EOL.'<script>'.PHP_EOL;
+ 		$html .= 'var uuid = $("#jsuuid").val()'.PHP_EOL;
+ 		$html .= '$uuid = uuid'.PHP_EOL;
+ 		$html .= 'if( $uuid != "" ) {'.PHP_EOL;
+ 		$html .= '$( "#jsswitchButton" ).append( "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<em>Content ID:</em> ")'.PHP_EOL;
+ 		$html .= '$( "#jsswitchButton" ).append( uuid )'.PHP_EOL;
+ 		$html .= '}'.PHP_EOL;
+ 		$html .= '</script>'.PHP_EOL;
+ 		return $html;
+
+    }
+
+}
